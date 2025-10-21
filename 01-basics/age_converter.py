64d658ddc716145b064c826ea4ee4e3e047f640b
Mathiu Osorio <matzap.ai@gmail.com>
2025-10-20 19:36:47 -0500

    feat: age to days converter

---
+++ 01-basics/age_converter.py
@@ -0,0 +1,7 @@
+print("---AGE TO DAYS CONVERTER---")
+
+age = int(input("Enter your age in years: "))
+
+age_days = age * 365
+
+print(f"You are {age} years old and have lived {age_days} days approximately")
